804d88798778185fd1192ef6dd249a3b81ddd216
Billie Devolder <devolder.billie96@gmail.com>
2019-12-21 11:46:28 +0100

    Add day 18 part 1

--- day18/solution.py
+++ day18/solution.py
@@ -1,79 +1,96 @@
+import time
 from collections import deque
+from functools import lru_cache
 
 from read_file.read_file import read_file
 
 lines = read_file("input.txt")[:-1]
-lines = """########################
-#...............b.C.D.f#
-#.######################
-#.....@.a.B.c.d.A.e.F.g#
-########################""".split("\n")
+# lines = """#################
+# #i.G..c...e..H.p#
+# ########.########
+# #j.A..b...f..D.o#
+# ########@########
+# #k.E..a...g..B.n#
+# ########.########
+# #l.F..d...h..C.m#
+# #################""".split("\n")
 
 world = [list(line) for line in lines]
 
 start_pos = None
-n_keys = 0
+keys = set()
 n_doors = 0
 key_positions = {}
 for r, row in enumerate(world):
     for c, char in enumerate(row):
         if char == "@":
             start_pos = (r, c)
+            key_positions[char] = (r, c)
         elif char.islower():
-            n_keys += 1
+            keys.add(char)
             key_positions[char] = (r, c)
         elif char.isupper():
             n_doors += 1
 
-print(start_pos, n_keys, n_doors)
+print(start_pos, keys, n_doors)
 
 
-def find_possible_keys(pos, opened_doors):
-    queue = deque([(pos, 0)])
-    visited = {pos}
-    possible_keys = []
+def find_distances(start_pos):
+    queue = deque([(start_pos, 0, set())])
+    visited = {start_pos}
+    distances = []
     while queue:
-        pos, distance = queue.popleft()
-        r, c = pos
+        start_pos, distance, doors_on_path = queue.popleft()
+        r, c = start_pos
+
+        current = world[r][c]
+        if current.isupper():
+            doors_on_path = doors_on_path | {current.lower()}
+
         for (dr, dc) in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             neighbor = world[r + dr][c + dc]
             neighbor_pos = (r + dr, c + dc)
-            if (neighbor in [".", "@"] or neighbor.upper() in opened_doors) and neighbor_pos not in visited:
-                queue.append((neighbor_pos, distance + 1))
+            if neighbor != "#" and neighbor_pos not in visited:
+                queue.append((neighbor_pos, distance + 1, doors_on_path))
                 visited.add(neighbor_pos)
-            elif neighbor.islower() and neighbor.upper() not in opened_doors:
-                possible_keys.append((neighbor, distance + 1))
-    return possible_keys
+                if neighbor.islower():
+                    distances.append((neighbor, distance + 1, doors_on_path))
+    return distances
 
 
-def find_shortest_path(world, start_pos):
-    lowest_steps = 10 ** 10
-
-    def back_track(pos, current_path, current_steps):
-        nonlocal lowest_steps
-
-        if current_steps + 6 > lowest_steps:
-            return
-
-        if len(current_path) == n_keys and current_steps < lowest_steps:
-            lowest_steps = current_steps
-            print(lowest_steps, current_path)
-
-        opened_doors = {key.upper() for key in current_path}
-        possible_keys = find_possible_keys(pos, opened_doors)
-
-        possible_steps_needed = [a[1] for a in possible_keys]
-        if not possible_steps_needed or max(possible_steps_needed) - min(
-                possible_steps_needed) + current_steps > lowest_steps:
-            return
-
-        possible_keys.sort(key=lambda a: a[1])
-        for (key, steps_needed) in possible_keys:
-            key_pos = key_positions[key]
-            back_track(key_pos, current_path + [key], current_steps + steps_needed)
-
-    back_track(start_pos, [], 0)
-    print(lowest_steps)
+key_to_distances = {}
+for key in ["@"] + list(keys):
+    key_to_distances[key] = find_distances(key_positions[key])
 
 
-find_shortest_path(world, start_pos)
+def find_shortest_steps(start_pos):
+    cache = {}
+
+    def calculate_required_steps(current_key, remaining_keys):
+        if not remaining_keys:
+            return 0
+
+        cache_key = current_key + "".join(remaining_keys)
+        if cache_key in cache:
+            return cache[cache_key]
+
+        lowest_required_steps = float("inf")
+        for (key, steps, required_opened_doors) in key_to_distances[current_key]:
+            if steps > lowest_required_steps:
+                continue
+            elif required_opened_doors & remaining_keys: # Not all doors have been opened
+                continue
+            elif key not in remaining_keys: # Key has already been selected
+                continue
+            else:
+                required_steps = calculate_required_steps(key, remaining_keys - {key}) + steps
+                lowest_required_steps = min(lowest_required_steps, required_steps)
+
+        cache[cache_key] = lowest_required_steps
+        return lowest_required_steps
+
+    return calculate_required_steps("@", keys)
+
+
+
+find_shortest_steps(start_pos)
